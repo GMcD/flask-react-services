b16d14d40a1fc953d9c8f766a24700f93a62579c
GMcD <gary.macdonald@projectscapa.com>
2020-09-07 08:20:18 +0100

    Added Proxy-Ssl and React environment setting.

--- containers/react/entrypoint.sh
+++ containers/react/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+yarn install
+
+# Patch Bug in legacy component..
+ln -sf /service/react/node_modules/react-cardstack/dist/CardStack.js /service/react/node_modules/react-cardstack/dist/Cardstack.js
+
 yarn start &
 
 tail -f /dev/null
